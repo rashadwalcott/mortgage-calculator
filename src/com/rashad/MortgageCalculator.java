1acc3fd05600c1c7db00bbc12934b3f389851dbe
Rashad <rashad.walcott15@gmail.com>
2021-02-02 11:07:41 -0500

    Extracting Duplicate Logic

--- src/com/rashad/MortgageCalculator.java
+++ src/com/rashad/MortgageCalculator.java
@@ -3,7 +3,7 @@ package com.rashad;
 public class MortgageCalculator {
     public final static byte MONTHS_IN_YEAR = 12;
     public final static byte PERCENT = 100;
-    
+
     private int principal;
     private float annualInterest;
     private byte years;
@@ -16,8 +16,8 @@ public class MortgageCalculator {
 
     public double calculateBalance(short numberOfPaymentsMade) {
 
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+        float monthlyInterest = getMonthlyInterest();
+        float numberOfPayments = getNumberOfPayments();
 
         double balance = principal
                 * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
@@ -26,16 +26,25 @@ public class MortgageCalculator {
         return balance;
     }
 
+
     public double calculateMortgage() {
 
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
-        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+        float monthlyInterest = getMonthlyInterest();
+        short numberOfPayments = (short) (getNumberOfPayments());
 
         double mortgage = principal
                 * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
                 / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
 
         return mortgage;
+    }
+
+    private int getNumberOfPayments() {
+        return years * MONTHS_IN_YEAR;
+    }
+
+    private float getMonthlyInterest() {
+        return annualInterest / PERCENT / MONTHS_IN_YEAR;
     }
 
     public byte getYears() {
